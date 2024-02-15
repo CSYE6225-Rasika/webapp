a249a43e2cc60274bff6b354caf29b1f7a10a479
rasika-NU <sahane.r@northeastern.edu>
2024-02-15 16:38:56 -0500

    Making changes for code to work with git action workflow

--- Application.py
+++ Application.py
@@ -12,7 +12,7 @@ from functools import wraps
 from datetime import datetime
 import base64
 import uuid
-from werkzeug.urls import quote
+from werkzeug.urls import unquote
 from urllib.parse import quote_plus
 
 
